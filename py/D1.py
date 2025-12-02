e5f98e0369acaaf6984f03eee54a882b12ba0012
yasminmuttalif-bit <yasminmuttalif@gmail.com>
2025-12-02 10:26:59 +0800

    first commit

---
+++ py/D1.py
@@ -0,0 +1,32 @@
+# name = "Yasmin"
+# age = 29
+# weight = 65.9
+# is_female = True
+
+# print(weight)
+# print(type(weight))
+
+# x = 10
+# y = 3
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y) # Floor division
+# print(x % y)  # Modulus
+# print(x ** y) # Exponentiation
+
+
+name = "Yasmin"
+age = 29
+height = 1.7
+is_student = True
+
+print(is_student)
+print(type(is_student))
+
+# Celcius = 25
+# Fahrenheit = (Celcius * 9/5 + 32)
+
+# print (Fahrenheit)
